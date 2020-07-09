5a3516b40fcb3dcf543ae1e64bdcbfb4f2a48dc2
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-07-09 01:32:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,26 +5,26 @@ end
 badges = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def batch_badge_creator(badges)
-result = []
-badges.each do |badge|
-  result << badge_maker(badge)
-end
-result
+  result = []
+  badges.each do |badge|
+    result << badge_maker(badge)
+  end
+  result
 end
 
 def assign_rooms(badges)
-result = []  
+  result = []  
   badges.each_with_index do |name, num|
-result << "Hello, #{name}! You'll be assigned to room #{num+1}!"
-end
-result
+    result << "Hello, #{name}! You'll be assigned to room #{num+1}!"
+  end
+  result
 end
 
 def printer(attendees)
   batch_badge_creator(attendees).each do |badge|
-  puts badge
+    puts badge
  end
   assign_rooms(attendees).each do |room|
     puts room
-end
+  end
 end
